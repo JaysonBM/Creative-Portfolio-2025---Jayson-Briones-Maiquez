4a2463229898d3687baa1945ae6ced5a6793f67f
JaysonBM <JaysonBMaiquez@gmail.com>
2024-03-23 15:11:54 -0400

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-$('#menu-icon').click(function () {
+function toggleMenu() {
     $('nav').toggleClass("navexpanded");
-    return false;
+}
+
+// Event listener for menu icon click
+$('#menu-icon').click(function (e) {
+    toggleMenu();
+    e.preventDefault(); // Prevent default behavior of the link
+});
+
+// Event listener for close icon click
+$('#close-icon').click(function (e) {
+    toggleMenu();
+    e.preventDefault(); // Prevent default behavior of the link
+});
+
+// Event listener for menu item click
+$('#click-function a').click(function (e) {
+    toggleMenu(); // Close the menu
 });
