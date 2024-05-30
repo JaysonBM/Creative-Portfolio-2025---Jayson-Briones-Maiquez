b76b60b012f483bcc5ae9ea5f911bb13eb3d7047
JaysonBM <JaysonBMaiquez@gmail.com>
2024-05-30 16:44:23 -0400

    .

--- js/script.js
+++ js/script.js
@@ -19,25 +19,58 @@ $('#click-function a').click(function (e) {
     toggleMenu(); // Close the menu
 });
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const galleryDiv = document.getElementById('gallery');
-    const childDivs = galleryDiv.querySelectorAll('.image-wrapper');
 
-    childDivs.forEach((div, index) => {
-        div.addEventListener('mouseover', () => {
-            childDivs.forEach((otherDiv, otherIndex) => {
-                if (index !== otherIndex) {
-                    otherDiv.style.backgroundColor = 'lightcoral';
-                }
-            });
-        });
 
-        div.addEventListener('mouseout', () => {
-            childDivs.forEach((otherDiv, otherIndex) => {
-                if (index !== otherIndex) {
-                    otherDiv.style.backgroundColor = 'lightblue';
-                }
-            });
-        });
+// hover.js
+document.addEventListener("DOMContentLoaded", function () {
+    const unityWrapper = document.querySelector('.unity-wrapper');
+    const bespokenWrapper = document.querySelector('.bespoken-wrapper');
+    const kingdomWrapper = document.querySelector('.kingdom-wrapper');
+    const momoWrapper = document.querySelector('.momo-wrapper');
+    const signageWrapper = document.querySelector('.signage-wrapper');
+
+    const unityimg = unityWrapper.querySelector('picture');
+    const unityh2 = unityWrapper.querySelector('h2');
+    const unityh3 = unityWrapper.querySelector('h3');
+    const unityp = unityWrapper.querySelector('p');
+    
+    const unityOriginal = unityWrapper.innerHTML;
+    const bespokenOriginal = bespokenWrapper.innerHTML;
+    const kingdomOriginal = kingdomWrapper.innerHTML;
+    const momoOriginal = momoWrapper.innerHTML;
+    const signageOriginal = signageWrapper.innerHTML;
+
+    unityWrapper.addEventListener('mouseenter', function () {
+        bespokenWrapper.innerHTML = '<img src="images/unity-mockup3.jpg" alt="Description of the image">';
+        kingdomWrapper.innerHTML = '<img src="images/MacBook Air PSD Mockup.jpg" alt="Description of the image">';
+        momoWrapper.innerHTML = '<img src="images/unity-mockup.jpg" alt="Description of the image">';
+        signageWrapper.innerHTML = '<img src="images/unity-mockup2.jpg" alt="Description of the image">';
+
+        unityWrapper.style.opacity = '0';
+        setTimeout(function () {
+            unityimg.style.display = 'none';
+            unityh2.style.display = 'block';
+            unityh3.style.display = 'block';
+            unityp.style.display = 'block';
+            unityWrapper.style.opacity = '1';
+        }, 300);
+    });
+    
+    unityWrapper.addEventListener('mouseleave', function () {
+        setTimeout(function () {
+        bespokenWrapper.innerHTML = bespokenOriginal;
+        kingdomWrapper.innerHTML = kingdomOriginal;
+        momoWrapper.innerHTML = momoOriginal;
+        signageWrapper.innerHTML = signageOriginal;
+
+        unityWrapper.style.opacity = '0';
+        
+            unityimg.style.display = 'block';
+            unityh2.style.display = 'none';
+            unityh3.style.display = 'none';
+            unityp.style.display = 'none';
+            unityWrapper.style.opacity = '1';
+        }, 300);
     });
 });
+
