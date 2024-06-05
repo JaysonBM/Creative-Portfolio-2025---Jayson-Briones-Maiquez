9f7f3b9b4fea521e9d877d3f2778315484cc52b6
JaysonBM <JaysonBMaiquez@gmail.com>
2024-06-05 18:07:40 -0400

    .

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ $('#click-function a').click(function (e) {
 
 
 
-// hover.js
 document.addEventListener("DOMContentLoaded", function () {
     const unityWrapper = document.querySelector('.unity-wrapper');
     const bespokenWrapper = document.querySelector('.bespoken-wrapper');
@@ -29,48 +28,102 @@ document.addEventListener("DOMContentLoaded", function () {
     const momoWrapper = document.querySelector('.momo-wrapper');
     const signageWrapper = document.querySelector('.signage-wrapper');
 
-    const unityimg = unityWrapper.querySelector('picture');
+    const unitydiv = unityWrapper.querySelector('picture');
     const unityh2 = unityWrapper.querySelector('h2');
     const unityh3 = unityWrapper.querySelector('h3');
     const unityp = unityWrapper.querySelector('p');
+    const unityimg = document.querySelector('.unityimg1');
+    const unityimg2 = document.querySelector('.unityimg2');
+    const unityimg3 = document.querySelector('.unityimg3');
+    const unityimg4 = document.querySelector('.unityimg4');
     
-    const unityOriginal = unityWrapper.innerHTML;
-    const bespokenOriginal = bespokenWrapper.innerHTML;
-    const kingdomOriginal = kingdomWrapper.innerHTML;
-    const momoOriginal = momoWrapper.innerHTML;
-    const signageOriginal = signageWrapper.innerHTML;
+    
+    
 
     unityWrapper.addEventListener('mouseenter', function () {
-        bespokenWrapper.innerHTML = '<img src="images/unity-mockup3.jpg" alt="Description of the image">';
-        kingdomWrapper.innerHTML = '<img src="images/MacBook Air PSD Mockup.jpg" alt="Description of the image">';
-        momoWrapper.innerHTML = '<img src="images/unity-mockup.jpg" alt="Description of the image">';
-        signageWrapper.innerHTML = '<img src="images/unity-mockup2.jpg" alt="Description of the image">';
 
         unityWrapper.style.opacity = '0';
         setTimeout(function () {
-            unityimg.style.display = 'none';
+            unitydiv.style.display = 'none';
             unityh2.style.display = 'block';
             unityh3.style.display = 'block';
             unityp.style.display = 'block';
+            unityimg.style.display = 'block';
+            unityimg2.style.display = 'block';
+            unityimg3.style.display = 'block';
+            unityimg4.style.display = 'block';
+
             unityWrapper.style.opacity = '1';
-        }, 300);
+        }, 1000);
     });
     
     unityWrapper.addEventListener('mouseleave', function () {
-        setTimeout(function () {
-        bespokenWrapper.innerHTML = bespokenOriginal;
-        kingdomWrapper.innerHTML = kingdomOriginal;
-        momoWrapper.innerHTML = momoOriginal;
-        signageWrapper.innerHTML = signageOriginal;
-
-        unityWrapper.style.opacity = '0';
         
-            unityimg.style.display = 'block';
+        unityWrapper.style.opacity = '0';
+        setTimeout(function () {
+            unitydiv.style.display = 'block';
             unityh2.style.display = 'none';
             unityh3.style.display = 'none';
             unityp.style.display = 'none';
+            unityimg.style.display = 'none';
+            unityimg2.style.display = 'none';
+            unityimg3.style.display = 'none';
+            unityimg4.style.display = 'none';
+
             unityWrapper.style.opacity = '1';
-        }, 300);
+        }, 1000);
     });
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const unityWrapper = document.querySelector('.unity-wrapper');
+//     const bespokenWrapper = document.querySelector('.bespoken-wrapper');
+//     const kingdomWrapper = document.querySelector('.kingdom-wrapper');
+//     const momoWrapper = document.querySelector('.momo-wrapper');
+//     const signageWrapper = document.querySelector('.signage-wrapper');
+
+//     const unityimg = unityWrapper.querySelector('picture');
+//     const unityh2 = unityWrapper.querySelector('h2');
+//     const unityh3 = unityWrapper.querySelector('h3');
+//     const unityp = unityWrapper.querySelector('p');
+    
+//     const unityOriginal = unityWrapper.innerHTML;
+//     const bespokenOriginal = bespokenWrapper.innerHTML;
+//     const kingdomOriginal = kingdomWrapper.innerHTML;
+//     const momoOriginal = momoWrapper.innerHTML;
+//     const signageOriginal = signageWrapper.innerHTML;
+
+//     unityWrapper.addEventListener('mouseenter', function () {
+//         bespokenWrapper.innerHTML = '<img src="images/unity-mockup3.jpg" alt="Description of the image">';
+//         kingdomWrapper.innerHTML = '<img src="images/MacBook Air PSD Mockup.jpg" alt="Description of the image">';
+//         momoWrapper.innerHTML = '<img src="images/unity-mockup.jpg" alt="Description of the image">';
+//         signageWrapper.innerHTML = '<img src="images/unity-mockup2.jpg" alt="Description of the image">';
+
+//         unityWrapper.style.opacity = '0';
+//         setTimeout(function () {
+//             unityimg.style.display = 'none';
+//             unityh2.style.display = 'block';
+//             unityh3.style.display = 'block';
+//             unityp.style.display = 'block';
+//             unityWrapper.style.opacity = '1';
+//         }, 1000);
+//     });
+    
+//     unityWrapper.addEventListener('mouseleave', function () {
+//         setTimeout(function () {
+//         bespokenWrapper.innerHTML = bespokenOriginal;
+//         kingdomWrapper.innerHTML = kingdomOriginal;
+//         momoWrapper.innerHTML = momoOriginal;
+//         signageWrapper.innerHTML = signageOriginal;
+
+//         unityWrapper.style.opacity = '0';
+        
+//             unityimg.style.display = 'block';
+//             unityh2.style.display = 'none';
+//             unityh3.style.display = 'none';
+//             unityp.style.display = 'none';
+//             unityWrapper.style.opacity = '1';
+//         }, 1000);
+//     });
+// });
 
