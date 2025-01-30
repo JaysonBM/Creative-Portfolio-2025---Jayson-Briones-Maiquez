16ba0028dc9517e434bf90f8e9a9a5538dad75b1
JaysonBM <JaysonBMaiquez@gmail.com>
2025-01-30 01:13:56 -0500

    .

--- js/about-script.js
+++ js/about-script.js
@@ -14,3 +14,24 @@ document.addEventListener('DOMContentLoaded', () => {
     updateTime();
     setInterval(updateTime, 1000); // Update the time every second
 });
+
+function toggleMenu() {
+    $('nav').toggleClass("navexpanded");
+}
+
+// Event listener for menu icon click
+$('#menu-icon').click(function (e) {
+    toggleMenu();
+    e.preventDefault(); // Prevent default behavior of the link
+});
+
+// Event listener for close icon click
+$('#close-icon').click(function (e) {
+    toggleMenu();
+    e.preventDefault(); // Prevent default behavior of the link
+});
+
+// Event listener for menu item click
+$('#click-function a').click(function (e) {
+    toggleMenu(); // Close the menu
+});
